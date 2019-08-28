f92fd6e1d6932ef0941e51d73453d336bce4efa0
Dariusz Korolczuk <dkorolczuk86@gmail.com>
2019-08-28 19:45:26 +0100

    MathOperations no main function

--- src/basic/MathOperations.java
+++ src/basic/MathOperations.java
@@ -1,11 +1,6 @@
 package src.basic;
 
 public class MathOperations {
-	
-	public static void main(String[] args){
-		String path = System.getProperty("java.library.path");
-		System.out.println(path);
-	}
     public int add(int one, int two){
         return one + two;
     }
